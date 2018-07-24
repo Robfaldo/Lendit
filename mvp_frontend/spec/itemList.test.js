c6354dc353432cb01b0724f13d0c1aa8ea4adca3
Rahul Rakshit <rahulrakshit@gmx.com>
2018-07-24 16:53:03 +0100

    unfinished ItemList

--- mvp_frontend/spec/itemList.test.js
+++ mvp_frontend/spec/itemList.test.js
@@ -7,25 +7,15 @@ const axios = require('axios');
 jest.mock('axios');
 Enzyme.configure({adapter: new Adapter()});
 
-beforeAll(() => {
-  const fakeResponse = {data: [
-    {_id: 1, itemName: 'scissors'},
-    {_id: 2, itemName: 'screwdriver'},
-    {_id: 3, itemName: 'a goat'}
-  ]}
-  axios.get.mockResolvedValue(fakeResponse);
-})
-
 describe('itemList', () => {
   it('is an unordered list', () => {
     const itemList = Enzyme.shallow(<ItemList />);
     expect(itemList.find('ul').length).toBe(1);
   })
 
-  it('the fake API actually works', () => {
-    axios.get('this will eventually be the database URL')
-      .then(res => {
-        expect(res.data).toEqual(fakeResponse.data);
-      })
+  it('renders three <li> items', () => {
+    const data = ['scissors', 'screwdriver', 'goat']
+    const itemList = Enzyme.shallow(<ItemList itemData={data}/>);
+    expect(itemList.find('li').length).toBe(3);
   })
 })
--- mvp_frontend/src/itemList.js
+++ mvp_frontend/src/itemList.js
@@ -1,8 +1,19 @@
 import React from 'react';
 
 class ItemList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.renderItem = (itemName, index) => {
+      return <li>key={index} value={itemName}</li>
+    }
+  }
   render() {
-    return <ul></ul>
+    return (
+      <ul>
+        {this.props.itemData
+          .map((itemName, index) => this.renderItem(itemName, index))}
+      </ul>
+    )
   }
 }
 
