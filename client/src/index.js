0fa4d8fb687f5e90fb31bd2836a92c38f518f636
Rahul Rakshit <rahulrakshit@gmx.com>
2018-07-25 17:06:50 +0100

    Create relatively pointless tests for ListingsPage

--- client/src/index.js
+++ client/src/index.js
@@ -1,39 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import ItemSubmitForm from '../src/itemSubmitForm';
-import ItemList from '../src/itemList';
+import ListingsPage from '../src/listingsPage';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {submitFormText: ''}
-    this.data = [
-      {_id: 'some sort of object id', itemName: 'ostrich egg'},
-      {_id: 'some other object id', itemName: 'iPhone charger cable'},
-      {_id: 'a third object id', itemName: 'neon green hair tie'}
-    ];
-    this.handleClick = () => {
-      alert(`I've been clicked with ${this.state.submitFormText}`);
-      this.setState({submitFormText: ''});
+const data = [
+  {_id: 'some sort of object id', itemName: 'ostrich egg'},
+  {_id: 'some other object id', itemName: 'iPhone charger cable'},
+  {_id: 'a third object id', itemName: 'neon green hair tie'}
+];
 
-    }
-    this.handleSubmitFormTextChange = (event) => {
-      this.setState({submitFormText: event.target.value})
-    }
-  }
-
-  render() {
-    return(
-      <div>
-        <ItemSubmitForm
-          handleClick={this.handleClick}
-          handleChange={this.handleSubmitFormTextChange}
-          value={this.state.submitFormText}
-        />
-        <ItemList itemsData={this.data}/>
-      </div>
-    )
-  }
-}
-
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<ListingsPage data={data} />, document.getElementById('root'));
