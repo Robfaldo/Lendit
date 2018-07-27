9136367ecb79c9313738f0f658d173a95af23276
Rahul Rakshit <rahulrakshit@gmx.com>
2018-07-27 11:24:11 +0100

    change configuration for db-related env variables

--- app.js
+++ app.js
@@ -9,14 +9,28 @@ app.use(bodyParser.urlencoded({extended: false}));
 
 var mongoose = require('mongoose');
 
-const databases = {
-    'lendit-test': 47101,
-    'lendit-prod': 47171,
-    'lendit-dev': 47001
+const databasesURLs = {
+    'lendit-test': `mongodb://${process.env.DB_USERNAME}:${process.env.DB_Password}@ds247101.mlab.com:47101/lendit-test`,
+    'lendit-prod': `mongodb://${process.env.DB_USERNAME}:${process.env.DB_Password}@ds247171.mlab.com:47171/lendit-prod`,
+    'lendit-dev': `mongodb://${process.env.DB_USERNAME}:${process.env.DB_Password}@ds247001.mlab.com:47001/lendit-dev`
 }
-const env = process.env.NODE_ENV || 'lendit-dev';
+const db_env = process.env.DB_ENV || 'production';
 
-var mongoDB = `mongodb://${process.env.DB_USERNAME}:${process.env.DB_Password}@ds247001.mlab.com:${databases[env.toString()]}/${env}`
+const mongoDB;
+if (db_env === 'production') {
+  mongoDB = databasesURLs.lendit-prod;
+}
+else if (db_env === 'development') {
+  mongoDB = databasesURLs.lendit-dev;
+}
+else if (db_env === 'test') {
+  mongoDB = databasesURLs.lendit-test;
+}
+else {
+  // just for debugging!
+  console.log("db_env has not been set!");
+}
+
 mongoose.connect(mongoDB);
 mongoose.Promise = global.Promise;
 var db = mongoose.connection;
