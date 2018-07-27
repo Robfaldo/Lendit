585d145d2fc73f542da2c161df7f1d2eb8b91349
Khie Biggs <khiebigzy@gmail.com>
2018-07-27 23:19:09 +0100

    Update user schema
    User schema previously did not have functions to compare the saved
    password and the input password, now it does

--- models/user.js
+++ models/user.js
@@ -1,10 +1,31 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 const UserSchema = new mongoose.Schema({
-    username: String,
-    password: String,
+    firstName: { type: String, unique: false, required: false },
+    username: { type: String, unique: false, required: false },
+    password: { type: String, unique: false, required: false },
 });
 
-let User = mongoose.model('User', UserSchema);
+UserSchema.methods = {
+    checkPassword: function(inputPassword) {
+        return bcrypt.compareSync(inputPassword, this.password)
+    },
+    hashPassword: plainTextPassword => {
+        return bcrypt.hashSync(plainTextPassword, 10)
+    }
+};
+
+UserSchema.pre('save', function(next) {
+    if (!this.password) {
+        console.log('=============No Password provided=========================');
+        next()
+    } else {
+        this.password = this.hashPassword(this.password);
+        next()
+    }
+});
+
+const User = mongoose.model('User', UserSchema);
 
 module.exports = User;
