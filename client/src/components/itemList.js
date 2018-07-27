15daafa73961413e4338244f9aadb6a1ad0530a3
Rahul Rakshit <rahulrakshit@gmx.com>
2018-07-27 14:42:42 +0100

    change listingsPage from ES7 to ES6

--- client/src/components/itemList.js
+++ client/src/components/itemList.js
@@ -2,25 +2,24 @@ import React from 'react';
 import Item from './item';
 
 class ItemList extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {data: this.props.itemsData}
+  constructor(props) {
+    super(props);
+    this.state = {data: this.props.itemsData}
+    this.renderItem = (itemData) => {
+      return <Item key={itemData._id} itemName={itemData.itemName} />
     }
-    componentWillReceiveProps(nextProps) {
-        this.setState({ data: nextProps.itemsData });
-    }
+  }
+  componentWillReceiveProps(nextProps) {
+    this.setState({ data: nextProps.itemsData });
+  }
 
-    renderItem = (itemData) => {
-        return <Item key={itemData._id} itemName={itemData.itemName} />
-    };
-
-    render() {
-        return (
-            <ul>
-                {this.state.data.map(item => this.renderItem(item))}
-            </ul>
-        )
-    }
+  render() {
+    return (
+      <ul>
+        {this.state.data.map(item => this.renderItem(item))}
+      </ul>
+    )
+  }
 }
 
 export default ItemList;
