28bc3cf64f67a46e5f56c9b8b4c3b3aee36ba5fb
Robert Faldo <robertfaldo@gmail.com>
2018-07-28 19:52:44 +0100

    Refactor database.js to be more concise
    Before this, we had a big if/else statement for connecting
    to the database. I have refactored to make it much more concise.
    The behaviour is the same.

--- database.js
+++ database.js
@@ -1,28 +1,12 @@
 var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
-const databasesURLs = {
-    'lendit-test': `mongodb://${process.env.DB_USERNAME}:${process.env.DB_Password}@ds247101.mlab.com:47101/lendit-test`,
-    'lendit-prod': `mongodb://${process.env.DB_USERNAME}:${process.env.DB_Password}@ds247171.mlab.com:47171/lendit-prod`,
-    'lendit-dev': `mongodb://${process.env.DB_USERNAME}:${process.env.DB_Password}@ds247001.mlab.com:47001/lendit-dev`
+const possibleUrls = {
+    'test': `mongodb://${process.env.DB_USERNAME}:${process.env.DB_Password}@ds247101.mlab.com:47101/lendit-test`,
+    'production': `mongodb://${process.env.DB_USERNAME}:${process.env.DB_Password}@ds247171.mlab.com:47171/lendit-prod`,
+    'development': `mongodb://${process.env.DB_USERNAME}:${process.env.DB_Password}@ds247001.mlab.com:47001/lendit-dev`
 }
 const db_env = process.env.DB_ENV || 'production';
+let databaseUrl = possibleUrls[db_env];
 
-let mongoDB;
-if (db_env === 'production') {
-  mongoDB = databasesURLs['lendit-prod'];
-}
-else if (db_env === 'development') {
-  mongoDB = databasesURLs['lendit-dev'];
-}
-else if (db_env === 'test') {
-  mongoDB = databasesURLs['lendit-test'];
-}
-else {
-  // just for debugging!
-  console.log("db_env has not been set!");
-}
-
-const databaseUrl = mongoDB;
-
-module.exports = databaseUrl
+module.exports = databaseUrl;
