5507407b76b42d639a136ea3230157896cd1cdd4
Rahul Rakshit <rahulrakshit@gmx.com>
2018-07-28 16:42:08 +0100

    Test drive handling submit

--- client/spec/userSignInForm.test.js
+++ client/spec/userSignInForm.test.js
@@ -5,20 +5,30 @@ import UserSignInForm from '../src/components/userSignInForm';
 
 Enzyme.configure({adapter: new Adapter()});
 
+const mockSubmit = jest.fn();
 const signInForm = Enzyme.shallow(
-  <UserSignInForm />
+  <UserSignInForm handleSubmit={mockSubmit} />
 );
 
 describe('SignInForm', () => {
-  it('has a sign in form', () => {
-    expect((signInForm).find('#signin-form').length).toEqual(1);
+  describe('html elements', () => {
+    it('has a sign in form', () => {
+      expect((signInForm).find('#signin-form').length).toEqual(1);
+    })
+
+    it('has a username field', () => {
+      expect((signInForm).find('#signin-username').length).toEqual(1);
+    })
+
+    it('has a password field', () => {
+      expect((signInForm).find('#signin-password').length).toEqual(1);
+    })
   })
 
-  it('has a username field', () => {
-    expect((signInForm).find('#signin-username').length).toEqual(1);
-  })
-
-  it('has a password field', () => {
-    expect((signInForm).find('#signin-password').length).toEqual(1);
+  describe('submit action', () => {
+    it('calls the handleSubmit callback when submitted', () => {
+      signInForm.find('#signin-form').simulate('submit');
+      expect(mockSubmit.mock.calls.length).toEqual(1);
+    })
   })
 })
--- client/src/components/userSignInForm.js
+++ client/src/components/userSignInForm.js
@@ -3,7 +3,7 @@ import React from 'react';
 class UserSignInForm extends React.Component {
   render() {
     return (
-      <form id="signin-form" className="sign-in-form">
+      <form id="signin-form" onSubmit={this.props.handleSubmit} className="sign-in-form">
         <input id="signin-username" type="text" placeholder="Username"/>
         <input id="signin-password" type="password" placeholder="Password"/>
       </form>
