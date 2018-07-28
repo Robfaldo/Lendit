d488b638d218a21c6d48f2746f1ae40a3f2380a9
Rahul Rakshit <rahulrakshit@gmx.com>
2018-07-28 15:52:36 +0100

    Test drive handling submit

--- client/src/components/userSignUpForm.js
+++ client/src/components/userSignUpForm.js
@@ -3,13 +3,14 @@ import React from 'react';
 class UserSignUpForm extends React.Component {
   render() {
     return (
-      <form id="signup-form" className="sign-up-form">
+      <form id="signup-form" onSubmit={this.props.handleSubmit} className="sign-up-form">
         <input id="signup-first-name" type="text" placeholder="First Name"/>
         <input id="signup-last-name" type="text" placeholder="Last Name"/>
         <input id="signup-username" type="text" placeholder="Username"/>
         <input id="signup-email" type="email" placeholder="Email" />
         <input id="signup-password" type="password" placeholder="Password" />
         <input id="signup-password-confirm" type="password" placeholder="Confirm password" />
+        <button type="submit" className="SubmitButton">Sign Up </button>
       </form>
     )
   };
