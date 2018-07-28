501c9b05b3746e20342e061d34b76b7a8ea3c2d4
Rahul Rakshit <rahulrakshit@gmx.com>
2018-07-28 16:46:43 +0100

    Add submit button

--- client/src/components/userSignInForm.js
+++ client/src/components/userSignInForm.js
@@ -6,6 +6,7 @@ class UserSignInForm extends React.Component {
       <form id="signin-form" onSubmit={this.props.handleSubmit} className="sign-in-form">
         <input id="signin-username" type="text" placeholder="Username"/>
         <input id="signin-password" type="password" placeholder="Password"/>
+        <button type="submit" className="SubmitButton">Sign In</button>
       </form>
     )
   }
