d59cb3b7e4757362d9fee82025ccd1a713abc7e8
Khie Biggs <khiebigzy@gmail.com>
2018-07-29 17:24:11 +0100

    intergrate busboy

---
+++ upload/index.js
@@ -0,0 +1,16 @@
+const Busboy = require('busboy');
+const express = require('express');
+const imageRoute = express();
+
+imageRoute.post('/upload', function (req, res, next) {
+    console.log("============ UPLOAD BEGIN ================");
+    let busboy = new Busboy({ headers: req.headers });
+    busboy.on('finish', function() {
+      console.log('Upload finished');
+      const file = req.files.image;
+      console.log(file);
+    });
+    req.pipe(busboy);
+  });
+
+module.exports = imageRoute;
