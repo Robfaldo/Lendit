615580a1334771191230b7b7f1af4e9397554bfc
Laura Petre <laura.d.petre@gmail.com>
2018-07-30 17:56:24 +0100

    Refactor item component as a function

--- client/src/components/item.js
+++ client/src/components/item.js
@@ -1,27 +1,24 @@
 import React from 'react';
 
-class Item extends React.Component {
-  constructor(props){
-    super(props);
-    this.state={
-      imageUrl : "https://makerslendit.s3.amazonaws.com/" + this.props.image + ".jpg",
-    }
-  }
-  render() {
-    return <li>
-            <div className="itemProps">
-              <p id="itemName">{this.props.itemName}</p>
-              <p id="itemDescription">{this.props.itemDescription}</p>
-              <img className="itemImage" style={{height:100, width: 100}} src={this.state.imageUrl} alt={this.props.itemName}/>
-            </div>
-            <form className="itemButtons" onSubmit={this.props.handleSubmit}>
-              <input name="itemId" type="hidden" value={this.props.itemId} />
-              <button type="submit" className="itemBorrow" name="itemBorrow">
-                Borrow
-              </button>
-            </form>
-          </li>
-  }
+function Item (props) {
+  let imageUrl = "https://makerslendit.s3.amazonaws.com/" + props.image + ".jpg";
+
+  return (
+      <li>
+        <div className="itemProps">
+          <p id="itemName">{props.itemName}</p>
+          <p id="itemDescription">{props.itemDescription}</p>
+          <img className="itemImage" style={{height:100, width: 100}} src={imageUrl} alt={props.itemName}/>
+        </div>
+        <form className="itemButtons" onSubmit={props.handleSubmit}>
+          <input name="itemId" type="hidden" value={props.itemId} />
+          <button type="submit" className="itemBorrow" name="itemBorrow">
+            Borrow
+          </button>
+        </form>
+      </li>
+  )
 }
+
 
 export default Item;
