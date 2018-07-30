4f558ec24cfdf3464beb0f1c4d865a7eee4fb1a6
Rahul Rakshit <rahulrakshit@gmx.com>
2018-07-30 21:27:34 +0100

    Refactor UserSignInForm into function

--- client/src/components/userSignInForm.js
+++ client/src/components/userSignInForm.js
@@ -1,28 +1,26 @@
 import React from 'react';
 
-class UserSignInForm extends React.Component {
-  render() {
-    return (
-      <form
-        id="signin-form"
-        onSubmit={this.props.handleSubmit}
-        className="signin-form">
-        Username: <input
-          id="signin-username"
-          name="username"
-          type="text"
-          placeholder="Username"
-        /><br />
-        Password: <input
-        id="signin-password"
-        name="password"
-        type="password"
-        placeholder="Password"
+function UserSignInForm(props) {
+  return (
+    <form
+      id="signin-form"
+      onSubmit={props.handleSubmit}
+      className="signin-form">
+      Username: <input
+        id="signin-username"
+        name="username"
+        type="text"
+        placeholder="Username"
       /><br />
-        <button type="submit" className="SubmitButton">Sign In</button>
-      </form>
-    )
-  }
+      Password: <input
+      id="signin-password"
+      name="password"
+      type="password"
+      placeholder="Password"
+    /><br />
+      <button type="submit" className="SubmitButton">Sign In</button>
+    </form>
+  )
 }
 
 export default UserSignInForm;
