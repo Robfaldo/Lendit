c1917375ab3efc6bdf629fc089c0a4644dbcbcb7
Rahul Rakshit <rahulrakshit@gmx.com>
2018-07-30 18:10:25 +0100

    Refactor itemList as function

--- client/src/components/itemList.js
+++ client/src/components/itemList.js
@@ -1,36 +1,30 @@
 import React from 'react';
 import Item from './item';
 
-class ItemList extends React.Component {
-  constructor(props) {
-    super(props);
-    this.renderItem = (itemData) => {
-      return (
-        <Item
-          key={itemData._id}
-          itemId={itemData._id}
-          itemName={itemData.itemName}
-          itemDescription={itemData.itemDescription}
-          image={itemData.image}
-          handleSubmit={this.handleSubmit}
-        />
-      )
-    }
-    this.handleSubmit=this.handleSubmit.bind(this);
-  }
+function renderItem(itemData) {
+  return (
+    <Item
+      key={itemData._id}
+      itemId={itemData._id}
+      itemName={itemData.itemName}
+      itemDescription={itemData.itemDescription}
+      image={itemData.image}
+      handleSubmit={handleSubmit}
+    />
+  )
+}
 
-  handleSubmit(event) {
-    console.log(event.target.itemId.value);
-    event.preventDefault();
-  }
+function handleSubmit(event) {
+  console.log(event.target.itemId.value);
+  event.preventDefault();
+}
 
-  render() {
-    return (
-      <ul>
-        {this.props.itemsData.map(item => this.renderItem(item))}
-      </ul>
-    )
-  }
+function ItemList(props) {
+  return (
+    <ul>
+      {props.itemsData.map(item => renderItem(item))}
+    </ul>
+  )
 }
 
 export default ItemList;
