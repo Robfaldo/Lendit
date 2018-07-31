64f09c8810d1baafa73156c50f6b140b04cca76d
Rahul Rakshit <rahulrakshit@gmx.com>
2018-07-31 12:20:15 +0100

    reset master to before all component refactoring

--- client/src/components/itemList.js
+++ client/src/components/itemList.js
@@ -1,25 +1,41 @@
 import React from 'react';
 import Item from './item';
 
-function ItemList(props) {
-  function renderItem(itemData) {
-    return (
-      <Item
-        key={itemData._id}
-        itemId={itemData._id}
-        itemName={itemData.itemName}
-        itemDescription={itemData.itemDescription}
-        image={itemData.image}
-        handleBorrowItem={props.handleBorrowItem}
-      />
-    )
+class ItemList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {data: this.props.itemsData};
+    this.renderItem = (itemData) => {
+      return (
+        <Item
+          key={itemData._id}
+          itemId={itemData._id}
+          itemName={itemData.itemName}
+          itemDescription={itemData.itemDescription}
+          image={itemData.image}
+          handleSubmit={this.handleSubmit}
+        />
+      )
+    }
+    this.handleSubmit=this.handleSubmit.bind(this);
   }
 
-  return (
-    <ul>
-      {props.itemsData.map(item => renderItem(item))}
-    </ul>
-  )
+  handleSubmit(event) {
+    console.log(event.target.itemId.value);
+    event.preventDefault();
+  }
+
+  componentWillReceiveProps(nextProps) {
+    this.setState({ data: nextProps.itemsData });
+  }
+
+  render() {
+    return (
+      <ul>
+        {this.state.data.map(item => this.renderItem(item))}
+      </ul>
+    )
+  }
 }
 
 export default ItemList;
