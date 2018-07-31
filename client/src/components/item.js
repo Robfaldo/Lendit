41801c0d60685d8c62cb9ef76989e5734e8fd583
Laura Petre <laura.d.petre@gmail.com>
2018-07-31 15:02:55 +0100

    Refactor image atributes

--- client/src/components/item.js
+++ client/src/components/item.js
@@ -12,7 +12,12 @@ class Item extends React.Component {
             <div className="itemProps">
               <p id="itemName">{this.props.itemName}</p>
               <p id="itemDescription">{this.props.itemDescription}</p>
-              <img className="itemImage" style={{height:100, width: 100}} src={this.state.imageUrl} alt={this.props.itemName}/>
+              <img
+                className="itemImage"
+                style={{height:100, width: 100}}
+                src={this.state.imageUrl}
+                alt={this.props.itemName}
+              />
             </div>
             <form className="itemButtons" onSubmit={this.props.handleSubmit}>
               <input name="itemId" type="hidden" value={this.props.itemId} />
