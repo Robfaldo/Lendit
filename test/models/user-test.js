adbd8c66c2486ca31232fde3dacc50bcc5b9de47
Robert Faldo <robertfaldo@gmail.com>
2018-08-01 11:14:12 +0100

    Remove before/after database hooks from User model tests
    Before this, the tests were connecting to and dropping
    before each test and then disconnecting after the tests.
    Now that all the tests in User model have been isolated and
    no longer need to connect to the database, I have removed these
    hooks

--- test/models/user-test.js
+++ test/models/user-test.js
@@ -5,16 +5,6 @@ const sinon = require('sinon');
 
 
 describe('User', () => {
-
-  beforeEach(async () => {
-    await mongoose.connect(`mongodb://${process.env.DB_USERNAME}:${process.env.DB_Password}@ds247001.mlab.com:47101/lendit-test`);
-    await mongoose.connection.db.dropDatabase();
-  });
-
-  afterEach(async () => {
-    await mongoose.disconnect();
-  });
-
   describe('#save', () => {
     it('creates a user with 10 points', async () => {
       const defaultValue = User.schema.paths.karmaPoints.defaultValue
