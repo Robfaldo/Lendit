543713acece690aecc60a3596d7296e5115f1127
Robert Faldo <robertfaldo@gmail.com>
2018-08-01 17:42:59 +0100

    Refactor auth-test for creating user with 10 points
    Before this, the logic for signing up a user was
    inside the test, after this commit it's extracted
    into the helper method signUserUp

--- test/routes/auth-test.js
+++ test/routes/auth-test.js
@@ -2,6 +2,7 @@ const {assert} = require('chai');
 const request = require('supertest');
 const mongoose = require('mongoose');
 const app = require('../../app');
+const {signUserUp} = require('../helper');
 
 
 describe('Server path /auth', () => {
@@ -17,20 +18,10 @@ describe('Server path /auth', () => {
 
   describe('/signup', () => {
     it('creates a user with 10 points', async () => {
-      const userToSignUp = {
-        "firstName": "Chris",
-        "lastName": "Terry",
-        "email": "chris@gmail.com",
-        "username": "chris555",
-        "password": "password"
-      };
-      const userResponse = await request(app)
-        .post('/auth/signup')
-        .type('form')
-        .send(userToSignUp);
-      const userSignedup = JSON.parse(userResponse.text);
+      const signedUpUser = await signUserUp({ "firstName": "Chris" });
+      const newUser = JSON.parse(signedUpUser.text);
 
-      assert.equal(userSignedup.karmaPoints, 10)
+      assert.equal(newUser.karmaPoints, 10)
     });
   });
 });
