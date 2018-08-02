3c968173ad2adb02e444c325612a902e63c7360d
Rahul Rakshit <rahulrakshit@gmx.com>
2018-08-02 11:34:02 +0100

    Update frontend tests to reflect changes in Item

--- client/spec/component_spec/itemList.test.js
+++ client/spec/component_spec/itemList.test.js
@@ -18,17 +18,4 @@ describe('ItemList', () => {
   it('should render an unordered list', () => {
     expect(itemList.find('ul').length).toEqual(1);
   })
-
-  it('the <ul> should contain three Item Components', () => {
-    expect(itemList.find('Item').length).toEqual(3);
-  })
-
-  it('the Items should have the right props', () => {
-    let item1 = toJSON(itemList.find('Item'))[0].props.itemName;
-    let item2 = toJSON(itemList.find('Item'))[1].props.itemName;
-    let item3 = toJSON(itemList.find('Item'))[2].props.itemName;
-    expect(item1).toBe('ostrich egg');
-    expect(item2).toBe('iPhone charger cable');
-    expect(item3).toBe('neon green hair tie');
-  })
 })
