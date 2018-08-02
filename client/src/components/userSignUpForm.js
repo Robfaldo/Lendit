05ec902f91fb22f7c9047aa949e5af7ff760734d
Laura Petre <laura.d.petre@gmail.com>
2018-08-02 18:38:09 +0100

    Remove old sign up form

--- client/src/components/userSignUpForm.js
+++ client/src/components/userSignUpForm.js
@@ -96,47 +96,6 @@ class UserSignUpForm extends React.Component {
           <button type="submit" class="waves-effect waves-light btn">Sign Up </button>
         </form>
       </div>
-      // <form
-      //   id="signup-form"
-      //   onSubmit={this.handleSubmit}
-      //   className="sign-up-form">
-      //
-      //   First Name <input
-      //     id="signup-first-name"
-      //     name="firstName"
-      //     type="text"
-      //     placeholder="First Name"
-      //   /><br />
-      //
-      //   Last Name <input
-      //     id="signup-last-name"
-      //     name="lastName"
-      //     type="text"
-      //     placeholder="Last Name"
-      //   /><br />
-      //
-      //   Username<input
-      //     id="signup-username"
-      //     name="username"
-      //     type="text"
-      //     placeholder="Username"
-      //   /><br />
-      //
-      //   Email<input
-      //     id="signup-email"
-      //     name="email"
-      //     type="email"
-      //     placeholder="Email"
-      //   /><br />
-      //
-      //   Password <input
-      //   id="signup-password"
-      //   name="password"
-      //   type="password"
-      //   placeholder="Password"
-      // />
-      //   <button type="submit" className="SubmitButton">Sign Up </button>
-      // </form>
     )
   };
 }
